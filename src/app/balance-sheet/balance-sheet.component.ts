de0e17be02b547816eb9420dfedee266efdfcf98
aparna <aparnaneelakandhan135@gmail.com>
2021-11-25 17:54:36 +0530

    jobss  commits

--- src/app/balance-sheet/balance-sheet.component.ts
+++ src/app/balance-sheet/balance-sheet.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { UserService } from '../user/user.service';
-import { HttpClient,HttpHeaders } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { FormBuilder, Validators } from '@angular/forms';
 import { Ledger } from '../user/login.interfaces';
 import { Observable } from 'rxjs';
@@ -15,24 +15,24 @@ import { BSFrom, CashFrom, CashSaleResponse, CreditPurchaseResponse, CreditSaleR
 })
 export class BalanceSheetComponent implements OnInit {
   BsForm = this.formBuilder.group({
-    date:["",Validators.required],
-    report_date: ["",Validators.required],
+    date: ["", Validators.required],
+    report_date: ["", Validators.required],
 
   });
 
   assetTotal = 0;
-LiabilityTotal=0;
+  LiabilityTotal = 0;
   Cash: CashSaleResponse[];
   PCash: PCashSaleResponse[];
-  credit:CreditSaleResponse[];
-  pcredit:CreditPurchaseResponse[];
+  credit: CreditSaleResponse[];
+  pcredit: CreditPurchaseResponse[];
   service: any;
 
   constructor(
-    private http:HttpClient,
+    private http: HttpClient,
     private salesservice: SalesService,
     private userservice: UserService,
-    private router:Router,
+    private router: Router,
     private formBuilder: FormBuilder
   ) { }
 
@@ -44,49 +44,49 @@ LiabilityTotal=0;
   }
 
   onSubmit(): void {
-    // this.calculsteLiability();
+    this.calculsteLiability();
     this.calculateAsset();
   }
 
   calculsteLiability() {
-    this.salesservice.balance_sheet(this.BsForm.value,).subscribe((data,)=>{
-      this.credit = data;
-      let total=0;
-            this.credit.forEach(element => {
-              this.LiabilityTotal += element.total3
-            });
+    // this.salesservice.balance_sheet(this.BsForm.value).subscribe((data) => {
+    //   this.credit = data;
+    //   let total = 0;
+    //   this.credit.forEach(element => {
+    //     this.LiabilityTotal += element.total3
+    //   });
 
-            });
+    // });
 
-      this.salesservice.tcrlb(this.BsForm.value,).subscribe((data,)=>{
-        this.pcredit = data;
-        let total=0;
-        this.pcredit.forEach(element => {
-          this.LiabilityTotal += element.total3
-        });
+    this.salesservice.tcrlb(this.BsForm.value).subscribe((data) => {
+      this.pcredit = data;
+      let total = 0;
+      this.pcredit.forEach(element => {
+        this.LiabilityTotal += element.total3
+      });
 
-        });
-    this.service.tcrlb(this.BsForm.value).subscribe((data,)=>{
-      console.log(data);
     });
+    // this.salesservice.tcrlb(this.BsForm.value).subscribe((data,) => {
+    //   console.log(data);
+    // });
 
   }
 
   calculateAsset() {
-    this.salesservice.lsb({}).subscribe((cash) => {
-      this.Cash = cash;
-      let total=0;
-        this.Cash.forEach(element => {
-          this.assetTotal += element.total3
-        });
-    });
+    // this.salesservice.lsb({}).subscribe((cash) => {
+    //   this.Cash = cash;
+    //   let total = 0;
+    //   this.Cash.forEach(element => {
+    //     this.assetTotal += Number(element.total3);
+    //   });
+    // });
 
-    this.salesservice.lslb(this.BsForm.value).subscribe((pcash)=>{
+    this.salesservice.lslb(this.BsForm.value).subscribe((pcash) => {
       this.PCash = pcash;
-      let total=0;
-        this.PCash.forEach(element => {
-          this.assetTotal += element.total3
-        });
+      let total = 0;
+      this.PCash.forEach(element => {
+        this.assetTotal += Number(element.total3);
+      });
     });
   }
 }
