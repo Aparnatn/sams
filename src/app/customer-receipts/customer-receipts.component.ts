928c15bf7fba9ea94dc913e7a0d37b70e43da31a
anjali666 <anjalimenon208@gmail.com>
2021-11-25 19:31:29 +0530

    reports commit6

--- src/app/customer-receipts/customer-receipts.component.ts
+++ src/app/customer-receipts/customer-receipts.component.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder } from '@angular/forms';
+import { FormBuilder, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { CustomerReceiptHistoryResponse } from '../reports/reports.interface';
 import { ReportsService } from '../reports/reports.service';
@@ -13,21 +13,26 @@ import { ReportsService } from '../reports/reports.service';
 export class CustomerReceiptsComponent implements OnInit {
 
   customerReceiptForm = this.formBuilder.group({
-    report_date:"",
+    date:['',Validators.required],
+    report_date:['',Validators.required],
    });
 
    receipt: CustomerReceiptHistoryResponse[];
   constructor(private http:HttpClient,private router:Router,private formBuilder: FormBuilder,private service:ReportsService,) { }
 
   ngOnInit(): void {
-    this.service.customerRcptHF({}).subscribe((data) => {
-      this.receipt = data;
-      console.log(data);
-    })
+    // this.service.customerRcptHF({}).subscribe((data) => {
+    //   this.receipt = data;
+    //   console.log(data);
+    // })
   }
   onSubmit(): void {
     this.service.customerRcptHF(this.customerReceiptForm.value,).subscribe((data,)=>{
       console.log(data);});
+      this.service.customerRcptHF({}).subscribe((data) => {
+        this.receipt = data;
+        console.log(data);
+      })
     // this.router.navigate(['/reports']);
   }
 }
